632afa91152d4a793f78d3ac3b8c0a57b13d2742
kyubhin <kyubhinhan@gmail.com>
2023-02-19 16:01:39 +0900

    Feat : 로컬 이미지 업로드
    이미지를 로컬에 저장하는 부분까지 완성

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -10,6 +10,7 @@ const Page = () => {
   const profileImage = searchParams.get("profileImage");
   const [nickname, setNickname] = useState("");
   const [nicknameCheck, setNicknameCheck] = useState("");
+  const [imageFile, setImageFile] = useState<File>();
 
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
@@ -17,7 +18,7 @@ const Page = () => {
 
   // 닉네임 중복 여부를 파악해줌
   const handleNicknameCheck = async () => {
-    const url = "http://localhost:3000/api/signincheck/nicknamecheck";
+    const url = "http://localhost:3000/api/signup/nicknamecheck";
     const { data } = await axios.post(url, {
       nickname,
     });
@@ -29,9 +30,32 @@ const Page = () => {
     }
   };
 
+  // 회원 가입 요청이 제출되었을 때 처리해줌
+  const handleSignupSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const targetData = new FormData();
+    targetData.append("email", String(email));
+    targetData.append("nickname", String(nickname));
+    targetData.append("profileImage", imageFile as File);
+
+    // 전송
+    const url = "http://localhost:3000/api/signup";
+    axios.post(url, targetData).then((res) => {
+      console.log(res);
+    });
+  };
+
+  // 사진이 입력되었을 때 처리를 해줌
+  const handleImageSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files !== null) {
+      setImageFile(e.target.files[0]);
+    }
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSignupSubmit}>
         <section>
           <label htmlFor="signup-email">이메일</label>
           <div id="signup-email">{email}</div>
@@ -50,7 +74,12 @@ const Page = () => {
         </section>
         <section>
           <label htmlFor="signup-profileimage">프로필 이미지</label>
-          <input id="signup-profileimage" type="file" />
+          <input
+            id="signup-profileimage"
+            type="file"
+            accept="image/*"
+            onChange={handleImageSubmit}
+          />
         </section>
         <button type="submit"> 제출</button>
       </form>
---
+++ pages/api/signup/index.ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import multer from "multer";
+import nextConnect from "next-connect";
+
+// Returns a Multer instance that provides several methods for generating
+// middleware that process files uploaded in multipart/form-data format.
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "./public/uploads",
+    filename: (req, file, cb) => cb(null, file.originalname),
+  }),
+});
+
+const apiRoute = nextConnect<NextApiRequest, NextApiResponse>({
+  // Handle any other HTTP method
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
+
+// Returns middleware that processes multiple files sharing the same field name.
+const uploadMiddleware = upload.single("profileImage");
+
+// Adds the middleware to Next-Connect
+apiRoute.use(uploadMiddleware);
+
+// Process a POST request
+apiRoute.post((req, res) => {
+  console.log(req.body);
+  res.status(200).json({ data: "success" });
+});
+
+export default apiRoute;
+
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
